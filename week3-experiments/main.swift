44bb115654bddcdb5c809890b97f13990a346eff
homebrewdev <xstall@yandex.ru>
2019-06-21 10:36:43 +0400

    Initial Commit

---
+++ week3-experiments/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  week3-experiments
+//
+//  Created by tiger on 21.06.2019.
+//  Copyright © 2019 homework. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
